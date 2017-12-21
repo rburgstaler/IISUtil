9c6c6360f16dc897453e5fbf078b72da433827e6
Rick Burgstaler <rburgstaler@yahoo.com>
2017-12-21 11:00:37 -0600

    Finish moving the common code to IISConfigTool

--- IISUtil/CommandParams.cs
+++ IISUtil/CommandParams.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using IISConfigTool;
 
 namespace IISUtil
 {
--- IISUtil/CommandProcessor.cs
+++ IISUtil/CommandProcessor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using System.IO;
+using IISConfigTool;
 
 namespace IISUtil
 {
