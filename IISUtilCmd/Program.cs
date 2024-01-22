05fd68cf579b444da908a3140c612dedf0689b43
Rick Burgstaler <rburgstaler@yahoo.com>
2024-01-22 15:00:01 -0600

    Add a command line app to properly execute the config updates from the command line.  The way it was being done using the form application was a hack that had issues.

---
+++ IISUtilCmd/Program.cs
@@ -0,0 +1,20 @@
+﻿using IISUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IISUtilCmd
+{
+    internal class IISUtilCmdProgram
+    {
+        static void Main(string[] args)
+        {
+            CommandProcessor proc = new CommandProcessor();
+            proc.ErrorOut = Console.Error.WriteLine;
+            proc.StatusOut = Console.Out.WriteLine;
+            proc.Run(args);
+        }
+    }
+}
