3282ebe70cfa8f51d66f62743aa13c924b26a5fc
Rick Burgstaler <rburgstaler@yahoo.com>
2024-04-20 07:42:04 -0500

    Finish the process of removing the IISconfigTool sub-module and replacing it with IISUtilLib

--- IISUtil/CommandParams.cs
+++ IISUtil/CommandParams.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using IISConfigTool;
+using IISConfigLib;
 using ACMEClientLib;
 
 namespace IISUtil
--- IISUtil/CommandProcessor.cs
+++ IISUtil/CommandProcessor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using System.IO;
-using IISConfigTool;
+using IISConfigLib;
 using ACMEClientLib;
 
 namespace IISUtil
