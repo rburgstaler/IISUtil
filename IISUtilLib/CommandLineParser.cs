244754a7bcebdce3f7e990b37bcc65a7aa0dcc2d
Rick Burgstaler <rburgstaler@yahoo.com>
2024-04-20 07:50:57 -0500

    Move CommandParams.cs, CommandProcessor.cs, and CommandLineParser.cs to the new IISUtilLib assembly

---
+++ IISUtilLib/CommandLineParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace IISUtil
+{
+    public class CommandLineParser
+    {
+        //Thanks to Thomas Petersson: http://stackoverflow.com/questions/298830/split-string-containing-command-line-parameters-into-string-in-c-sharp 
+        public static String[] GetArguments(String CmdLine)
+        {
+            //Does not work properly with leading space
+            String cmdtxt = CmdLine.Trim();
+            String re = @"\G(""((""""|[^""])+)""|(\S+)) *";
+            MatchCollection ms = Regex.Matches(cmdtxt, re);
+            return ms.Cast<Match>().Select(m => Regex.Replace(m.Groups[2].Success ? m.Groups[2].Value : m.Groups[4].Value, @"""""", @"""")).ToArray();
+
+        }
+    }
+}
