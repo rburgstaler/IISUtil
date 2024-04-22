962756255325bb9cfded2ed17d37be94e124cf08
Rick Burgstaler <rburgstaler@yahoo.com>
2024-04-21 22:16:54 -0500

    Move in logic for installing a cert into the Windows Certificate store

--- IISUtilLib/SSLCertificates.cs
+++ IISUtilLib/SSLCertificates.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ACMEClientLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
@@ -59,6 +61,71 @@ namespace IISUtilLib
         public static void GetInstalledCertificates(SSLOutputMessage msgOutput)
         {
             (new SSLCertificates()).PrintOutAllCerts(msgOutput);
+        }
+
+        //A good default for certificateStore is WebHosting
+        public static byte[] InstallCertificate(string pfxFilename, string PFXPassword, DNSList DnsIdentifiers, String certificateStore, Action<string> StatusMsg)
+        {
+            byte[] retVal = new byte[0];
+            X509Store store = null;
+            X509Certificate2 certificate = null;
+
+            try
+            {
+                store = new X509Store(certificateStore, StoreLocation.LocalMachine);
+                store.Open(OpenFlags.OpenExistingOnly | OpenFlags.ReadWrite);
+            }
+            catch (CryptographicException)
+            {
+                store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+                store.Open(OpenFlags.OpenExistingOnly | OpenFlags.ReadWrite);
+            }
+            catch (Exception ex)
+            {
+                StatusMsg($"Error encountered while opening certificate store. Error: {ex.Message}");
+                throw new Exception(ex.Message);
+            }
+
+            StatusMsg($"Opened Certificate Store {store.Name}");
+            try
+            {
+                X509KeyStorageFlags flags = X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet;
+
+                //Set to be exportable
+                StatusMsg($" Set private key exportable");
+                StatusMsg("Set private key exportable");
+                flags |= X509KeyStorageFlags.Exportable;
+
+                // See http://paulstovell.com/blog/x509certificate2
+                certificate = new X509Certificate2(pfxFilename, PFXPassword, flags);
+
+                StringBuilder certName = new StringBuilder();
+                certName.Append(DnsIdentifiers.First());
+                if (DnsIdentifiers.Count > 1) certName.Append($"(+{DnsIdentifiers.Count - 1})");
+                certName.Append(" Expires: ").Append(certificate.NotAfter.ToString("yyyy/MM/dd HH:mm:ss"));
+                certificate.FriendlyName = certName.ToString();
+                StatusMsg(certificate.FriendlyName);
+
+                StatusMsg("Common Name: " + certificate.SubjectName.Name);
+                StatusMsg("Certificate hash: " + ByteArrayToHexString(certificate.GetCertHash()));
+
+
+
+                StatusMsg("Adding Certificate to Store");
+                store.Add(certificate);
+
+
+
+                retVal = certificate.GetCertHash();
+
+                StatusMsg("Closing Certificate Store");
+            }
+            catch (Exception ex)
+            {
+                StatusMsg($"Error saving certificate {ex.Message}");
+            }
+            store.Close();
+            return retVal;
         }
     }
 }
